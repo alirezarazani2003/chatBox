55ff5ec5dc027fcf28b79b08ca4349dc5037b6b1
alirezarazani2003 <alirezarazani3185@gmail.com>
2023-11-03 19:23:20 +0330

    athentication4-api2

--- athentication/serializer.py
+++ athentication/serializer.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['personalID', 'password','role','workshop']
+        fields = '__all__'
--- athentication/views.py
+++ athentication/views.py
@@ -11,70 +11,47 @@ from . import serializer
 # Create your views here.
 @api_view(['POST','GET'])
 def register(request:Request):
-    if request.method == "POST":
-        personalID = request.POST["personalID"]
-        workshop = request.POST["workshop"]
-        email = request.POST["email"]
-        role = request.POST["role"]
-        if username == "" or email =="":
-            return Response({"message": "Invalid username or email"},status.HTTP_200_OK)
-        pass
-#         #API
-            
-            
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password == "" or confirmation =="":
-            return Response({"message": "password should be exist!"},status.HTTP_200_OK)
-        
-        if password != confirmation:
-            return Response({"message": "Passwords must match."})
-            # API
-
-        # Attempt to create new user
-        try:
-            user = models.User.objects.create_user(personalID,email, password,role,workshop)
-            user.save()
-        except IntegrityError:
+    if request.method == 'POST':
+        print(request.data)
+        serializerr = serializer.UserSerializer(data=request.data)
+        try :
+            if serializerr.is_valid():
+                serializerr.save()
+        except :
             return Response({"message": "Username already taken."},status.HTTP_200_OK)
-            #API
-        login(request, user)
-    else:
-        return Response({"message": "Username already taken."},status.HTTP_200_OK)
-        #API
-    
+    elif request.method == 'GET':
+        ls = models.User.objects.all()
+        user_serializer = serializer.UserSerializer(ls, many=True)
+        return Response(user_serializer.data,status.HTTP_200_OK)
+    return Response(None)
 
+@api_view(['GET','POST'])
+def login_view(request : Request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        # personalID = request.POST["personalID"]
+        # password = request.POST["password"]
+        personalID = request.data['personalID']
+        password = request.data['password']
+        user = authenticate(request, personalID=personalID, password=password)
 
-def login_view(request):
-#     if request.method == "POST":
+        # Check if authentication successful
+        if user is not None:
+           login(request, user)
+           return Response(True)
 
-#         # Attempt to sign user in
-#         personalID = request.POST["personalID"]
-#         password = request.POST["password"]
-#         user = authenticate(request, personalID=personalID, password=password)
-
-#         # Check if authentication successful
-#         if user is not None:
-#             # login(request, user)
-            pass
-            
-#             # return HttpResponseRedirect(reverse("index"))
-#         else:
-#             # return render(request, "auctions/login.html", {
-#             #     "message": "Invalid username and/or password."
-#             # })
-#             pass
-#             #API
-#     else:
-#         # return render(request, "auctions/login.html")
-#         #API
-#         pass
-    
-def logout_view(request):
-#     logout(request)
-    pass
-#     #API
+        else:
+            return Response(None)
+    if request.method =='GET':
+        # ls = models.User.objects.all()
+        # user_serializer = serializer.UserSerializer(ls, many=True)
+        # return Response(user_serializer.data,status.HTTP_200_OK)
+        return Response('nothing')
+    return Response(None)
+# def logout_view(request):
+# #     logout(request)
+#     pass
+# #     #API
 
 @api_view(['GET'])
 def test(request : Request):
